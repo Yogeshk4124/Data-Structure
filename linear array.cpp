2ea886534c2946deae49e5b9d4a15e2d2baf14fe
Yogesh Kumar <55343959+Yogeshk4124@users.noreply.github.com>
2019-11-16 23:05:17 +0530

    Update linear array.cpp
    Write a menu Driven program that implement following on a linear array:
    1.Insert a new Element
    2.Delete an element from a given position
    3.To display the elements of the linear array

--- linear array.cpp
+++ linear array.cpp
@@ -19,28 +19,28 @@ int main(){
 			switch(c)
 			{
 			case 1://Insert
-            cout<<"\nEnter element:\n";
-				    cin>>a[++p];
-					  break;
+            			cout<<"\nEnter element:\n";
+				cin>>a[++p];
+				break;
 			case 2://Delete
-          cout<<"\nEnter the position of element:";
-					cin>>d;
-					if(d>p)
-					cout<<"Position Doesn't Exist\n";
-					else
-					for(int i=d;i<p;i++)
+         			 cout<<"\nEnter the position of element:";
+				 cin>>d;
+				 if(d>p)
+			         cout<<"Position Doesn't Exist\n";
+				 else
+				 for(int i=d;i<p;i++)
 					{
 						a[i]=a[i+1];
 					}
-					p--;
-					break;
+				  p--;
+				  break;
 			case 3://Display 
-          cout<<"\nElements:";
-					for(int i=0;i<=p;i++)
-					cout<<a[i]<<" ";
-					cout<<endl;
-					break;
-			default:"Invalid Option\n";
+          			cout<<"\nElements:";
+				for(int i=0;i<=p;i++)
+				cout<<a[i]<<" ";
+				cout<<endl;
+				break;
+			default:cout<<"Invalid Option\n";
 			
 	}
 cout<<"Want to Continue(y/n):";
