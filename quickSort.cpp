8643299c0dd18276c845800c2d6f2f76387ba6c1
Yogesh Kumar <55343959+Yogeshk4124@users.noreply.github.com>
2019-11-17 14:35:31 +0530

    Create quickSort.cpp

---
+++ quickSort.cpp
@@ -0,0 +1,52 @@
+/*
+QUICKSORT
+MADE BY:YOGESH KUMAR
+*/
+#include<iostream>
+using namespace std;
+
+int partition(int *a,int s,int e)
+{
+	int i=s-1,j=s;
+	int pivot=a[e];
+	for(j=s;j<e;j++)
+	{
+		if(a[j]<=pivot)
+		{
+		
+		i++;
+		swap(a[i],a[j]);
+		}	
+	}
+		swap(a[i+1],a[e]);
+	return i+1;
+}
+void quickSort(int *a,int s,int e)
+{
+	
+	if(s>=e)
+	{
+	return;
+	}
+	int p=partition(a,s,e);
+	
+	quickSort(a,s,p-1);
+	quickSort(a,p+1,e);
+}
+
+int main(){
+	int a[100],n;
+	cout<<"Enter a array size:";
+	cin>>n;
+	cout<<"Enter Element:\n";
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	quickSort(a,0,n-1);
+	cout<<"Sorted Data:";
+	for(int i=0;i<n;i++)
+	{
+	cout<<a[i]<<" ";
+	}
+}
